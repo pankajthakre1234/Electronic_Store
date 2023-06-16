d844292d26b113e05ffe42227a161f74c60bcccf
pankajthakre1234 <pankajthakre786@gmail.com>
2023-06-16 15:12:25 +0530

    create Product service code push by pankaj

---
+++ src/main/java/com/lcwd/electronic/store/service/ProductService.java
@@ -0,0 +1,32 @@
+package com.lcwd.electronic.store.service;
+
+import com.lcwd.electronic.store.dto.ProductDto;
+import com.lcwd.electronic.store.helper.PageableResponse;
+
+import java.util.List;
+
+public interface ProductService {
+
+//    create
+    ProductDto create(ProductDto productDto);
+
+//    update
+    ProductDto update(ProductDto productDto,Integer productId);
+
+//    delete
+    void delete(Integer productId);
+
+//    get Single
+    ProductDto getSingle(Integer productId);
+
+//    get All
+    PageableResponse<ProductDto> getAll(int pageNumber, int pageSize, String sortBy, String sortDir);
+
+//    get All Live
+    PageableResponse<ProductDto> getAllLive(int pageNumber, int pageSize, String sortBy, String sortDir);
+
+//    search product
+PageableResponse<ProductDto> searchByTitleContaining(String subTitle,int pageNumber, int pageSize, String sortBy, String sortDir);
+
+}
+
