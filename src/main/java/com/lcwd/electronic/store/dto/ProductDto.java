0d9242496e11485bdd9bad6947c8af56aa769ade
pankajthakre1234 <pankajthakre786@gmail.com>
2023-06-27 17:11:16 +0530

    test delete method code push by pankaj

--- src/main/java/com/lcwd/electronic/store/dto/ProductDto.java
+++ src/main/java/com/lcwd/electronic/store/dto/ProductDto.java
@@ -3,6 +3,8 @@ package com.lcwd.electronic.store.dto;
 import com.lcwd.electronic.store.validate.ImageNameValid;
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Date;
 @Getter
 @Setter
@@ -13,8 +15,12 @@ public class ProductDto {
 
     private Integer productId;
 
+    @NotEmpty(message = "title must Not be Empty")
+    @Size(min = 4,max = 100,message = "Title minimum 2 character")
     private String title;
 
+    @NotEmpty(message = "Must Not Be Empty")
+    @Size(max = 200,message = "Description is Required..!!")
     private String description;
 
     private Date addedDate;
