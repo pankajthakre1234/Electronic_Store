f385ce726de40f4e7cc18625fb31fabe7eb716ff
pankajthakre1234 <pankajthakre786@gmail.com>
2023-07-05 16:58:14 +0530

    category update Method Testing done by Test Class Method Is done code push by pankaj

--- src/test/java/com/lcwd/electronic/store/services/CategoryServiceTest.java
+++ src/test/java/com/lcwd/electronic/store/services/CategoryServiceTest.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.Optional;
+
 @SpringBootTest
 public class CategoryServiceTest {
 
@@ -28,24 +30,44 @@ public class CategoryServiceTest {
     Category category;
 
     CategoryDto categoryDto;
+
     @BeforeEach
-    public void init()
-    {
-        category =Category.builder()
+    public void init() {
+        category = Category.builder()
                 .title("Mobiles")
                 .description("This is Mobile Testing Create Method")
                 .categoryImage("abc.png")
                 .build();
     }
 
-//    create Category
+    //    create Category
     @Test
-    public void createCategory_Test()
-    {
+    public void createCategory_Test() {
         Mockito.when(this.repository.save(Mockito.any())).thenReturn(category);
 
         CategoryDto category1 = this.categoryService.createCategory(mapper.map(category, CategoryDto.class));
 
         Assertions.assertNotNull(category1);
+    }
+
+//    update category
+    @Test
+    public void updateCategory_Test()
+    {
+        categoryDto = CategoryDto.builder()
+                .title("Cars")
+                .description("This is Cars Testing Update Method")
+                .categoryImage("abc.png")
+                .build();
+
+        Integer catId=1;
+
+        Mockito.when(this.repository.findById(Mockito.anyInt())).thenReturn(Optional.ofNullable(category));
+        Mockito.when(this.repository.save(Mockito.any())).thenReturn(category);
+        CategoryDto updateCategory = this.categoryService.updateCategory(categoryDto, catId);
+
+        Assertions.assertNotNull(updateCategory);
+
+        Assertions.assertEquals(updateCategory.getTitle(),category.getTitle());
     }
 }
