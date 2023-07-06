048061bafde87cb4229471d759b4f86b2184075d
pankajthakre1234 <pankajthakre786@gmail.com>
2023-07-06 17:32:03 +0530

    Get All Product Testing Method is Successfully done code push by pankaj

--- src/test/java/com/lcwd/electronic/store/services/ProductServiceTest.java
+++ src/test/java/com/lcwd/electronic/store/services/ProductServiceTest.java
@@ -2,6 +2,7 @@ package com.lcwd.electronic.store.services;
 
 import com.lcwd.electronic.store.dto.ProductDto;
 import com.lcwd.electronic.store.entity.Product;
+import com.lcwd.electronic.store.helper.PageableResponse;
 import com.lcwd.electronic.store.repository.ProductRepository;
 import com.lcwd.electronic.store.service.ProductService;
 import org.junit.jupiter.api.Assertions;
@@ -12,8 +13,13 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -106,5 +112,54 @@ public class ProductServiceTest {
         Mockito.verify(repository,Mockito.timeout(1)).delete(product);
 
     }
+
+//    Get All Product
+    @Test
+    public void getAllProduct_Test()
+    {
+        Product product= Product.builder()
+            .title("Iphone 14 Pro Max")
+            .description("This Is Updated Apple Company Best Product Testing")
+            .price(140000)
+            .discountedPrice(138000)
+            .productImageName("iphone.png")
+            .quantity(50)
+            .addedDate(new Date())
+            .stock(true)
+            .live(true)
+            .build();
+
+        Product product1= Product.builder()
+                .title("Samsung S23 Ultra")
+                .description("This Is Samsung Company Best Product Testing")
+                .price(150000)
+                .discountedPrice(148000)
+                .productImageName("samsung.png")
+                .quantity(70)
+                .addedDate(new Date())
+                .stock(true)
+                .live(true)
+                .build();
+
+        Product product2= Product.builder()
+                .title("One Plus 11R 5G")
+                .description("This Is One Plus Company Best Product Testing")
+                .price(100000)
+                .discountedPrice(98000)
+                .productImageName("one.png")
+                .quantity(25)
+                .addedDate(new Date())
+                .stock(true)
+                .live(true)
+                .build();
+
+        List<Product> allUsers= Arrays.asList(product,product1,product2);
+        Page<Product> page= new PageImpl<>(allUsers);
+        Mockito.when(this.repository.findAll((Pageable) Mockito.any())).thenReturn(page);
+        PageableResponse<ProductDto> dto = this.productService.getAll(2, 3, "title", "asc");
+        Assertions.assertNotNull(dto);
+        Assertions.assertEquals(3,dto.getContent().size());
+    }
+
 
 }
