3936a30e1c5c9bfefe04a15359044c2ecbbcb286
pankajthakre1234 <pankajthakre786@gmail.com>
2023-07-07 16:51:45 +0530

    creating a controller testing class is  done code push by pankaj

---
+++ src/test/java/com/lcwd/electronic/store/controllers/UserControllerTest.java
@@ -0,0 +1,25 @@
+package com.lcwd.electronic.store.controllers;
+
+import com.lcwd.electronic.store.service.UserServiceI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserControllerTest {
+
+    @Autowired
+    private UserServiceI userServiceI;
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
