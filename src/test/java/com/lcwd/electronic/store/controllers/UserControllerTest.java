bf949d87e4088931ff60b1410dd0f90d99436edb
pankajthakre1234 <pankajthakre786@gmail.com>
2023-07-10 16:05:15 +0530

    complete the user controller's get User By Email api testing is done code push by pankaj

--- src/test/java/com/lcwd/electronic/store/controllers/UserControllerTest.java
+++ src/test/java/com/lcwd/electronic/store/controllers/UserControllerTest.java
@@ -3,9 +3,11 @@ package com.lcwd.electronic.store.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lcwd.electronic.store.dto.UserDto;
 import com.lcwd.electronic.store.entity.User;
+import com.lcwd.electronic.store.helper.ApiResponse;
 import com.lcwd.electronic.store.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.DoNotMock;
 import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.Arrays;
 import java.util.List;
 
@@ -49,6 +52,7 @@ public class UserControllerTest {
                 .imageName("abc.jpg")
                 .password("pankaj12")
                 .build();
+
     }
 
     private String convetObjectToJsonString(Object user) {
@@ -66,8 +70,7 @@ public class UserControllerTest {
 //    create user
 
     @Test
-    public void createUser_Test() throws Exception
-    {
+    public void createUser_Test() throws Exception {
         UserDto dto = this.mapper.map(user, UserDto.class);
         Mockito.when(this.userService.saveUser(Mockito.any())).thenReturn(dto);
         this.mockMvc.perform(MockMvcRequestBuilders.post("/api/user")
@@ -79,38 +82,36 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.name").exists());
     }
 
-//    update user
+    //    update user
     @Test
-    public void updateUser_Test() throws Exception
-    {
-        Integer userId=12;
+    public void updateUser_Test() throws Exception {
+        Integer userId = 12;
         UserDto userDto = this.mapper.map(user, UserDto.class);
-        Mockito.when(this.userService.updateUser(Mockito.any(),Mockito.anyInt())).thenReturn(userDto);
-        this.mockMvc.perform(MockMvcRequestBuilders.put("/api/"+userId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(convetObjectToJsonString(user))
-                .contentType(MediaType.APPLICATION_JSON))
+        Mockito.when(this.userService.updateUser(Mockito.any(), Mockito.anyInt())).thenReturn(userDto);
+        this.mockMvc.perform(MockMvcRequestBuilders.put("/api/" + userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(convetObjectToJsonString(user))
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name").exists());
     }
 
-//    get All users
+    //    get All users
     @Test
-    public void getAllUsers_Test() throws Exception
-    {
+    public void getAllUsers_Test() throws Exception {
         UserDto userDto = this.mapper.map(user, UserDto.class);
         UserDto userDto1 = UserDto.builder().name("Pankaj").email("pankajthakre@gmail.com").gender("male").password("pankaj1").about("This testing is for get All users").build();
         UserDto userDto2 = UserDto.builder().name("Ashish").email("ashish@gmail.com").gender("male").password("ashsih").about("This testing is for get All users").build();
         UserDto userDto3 = UserDto.builder().name("Amrut").email("amrut@gmail.com").gender("male").password("amrutt").about("This testing is for get All users").build();
         UserDto userDto4 = UserDto.builder().name("Ramesh").email("ramesh@gmail.com").gender("male").password("ramesh").about("This testing is for get All users").build();
 
-        List<UserDto> userList=Arrays.asList(userDto1,userDto2,userDto3,userDto4);
+        List<UserDto> userList = Arrays.asList(userDto1, userDto2, userDto3, userDto4);
         Mockito.when(this.userService.getAllUsers()).thenReturn(userList);
 
         this.mockMvc.perform(MockMvcRequestBuilders.get("/api/users")
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
@@ -120,16 +121,29 @@ public class UserControllerTest {
     @Test
     public void getSingleUser_Test() throws Exception
     {
-        Integer userId=2;
+        Integer userId = 2;
 
         UserDto userDto = this.mapper.map(user, UserDto.class);
 
         Mockito.when(this.userService.getSingleUserById(Mockito.any())).thenReturn(userDto);
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/user/"+userId)
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/user/" + userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+//   get user BYEmail
+    @Test
+    public void getUserByEmail_Test() throws Exception
+    {
+        UserDto userDto = this.mapper.map(user, UserDto.class);
+        String email="pankajthakre@gmail.com";
+        Mockito.when(this.userService.getUserByEmail(Mockito.anyString())).thenReturn(userDto);
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/user/"+email)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
-
 }
