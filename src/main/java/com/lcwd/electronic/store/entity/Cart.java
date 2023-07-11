e01ecac593983ffe943edc967ba611d90095a0d2
pankajthakre1234 <pankajthakre786@gmail.com>
2023-07-11 17:50:08 +0530

    create tables in database CartItem module and cart module is done code push by pankaj

--- src/main/java/com/lcwd/electronic/store/entity/Cart.java
+++ src/main/java/com/lcwd/electronic/store/entity/Cart.java
@@ -1,4 +1,33 @@
 package com.lcwd.electronic.store.entity;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Data
+@Builder
+@Entity
+@Table(name = "cart")
 public class Cart {
+
+    @Id
+    private Integer cartId;
+
+    @Column(name = "cart_added_date")
+    private Date createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "user_Id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private List<CartItem> itemList= new ArrayList<>();
 }
--- src/main/java/com/lcwd/electronic/store/entity/CartItem.java
+++ src/main/java/com/lcwd/electronic/store/entity/CartItem.java
@@ -1,4 +1,35 @@
 package com.lcwd.electronic.store.entity;
 
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Builder
+@Entity
+@Table(name = "cart_item")
 public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer cartItemId;
+
+    @OneToOne
+    @JoinColumn(name = "product_Id")
+    private Product product;
+
+    @Column(name = "cartItem_quantity")
+    private int quantity;
+
+    @Column(name = "total_price")
+    private int totalPrice;
+
+    @ManyToOne
+    private  Cart cart;
 }
