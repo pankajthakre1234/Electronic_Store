255efe8c4730d85eb452a21e02758a4605726820
pankajthakre1234 <pankajthakre786@gmail.com>
2023-07-11 16:28:32 +0530

    testing Product controllers create Product test cases Testing is done code push by pankaj

--- src/test/java/com/lcwd/electronic/store/controllers/ProductControllerTest.java
+++ src/test/java/com/lcwd/electronic/store/controllers/ProductControllerTest.java
@@ -1,12 +1,24 @@
 package com.lcwd.electronic.store.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lcwd.electronic.store.dto.ProductDto;
 import com.lcwd.electronic.store.entity.Product;
 import com.lcwd.electronic.store.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,17 +30,50 @@ public class ProductControllerTest
     @Autowired
     private ModelMapper mapper;
 
+    @Autowired
+    private MockMvc mockMvc;
+
     private Product product;
 
+    @BeforeEach
     public void init()
     {
         product=Product.builder()
-                .title("")
+                .title("Iphone 13")
+                .description("This Is Apple Company Products and Launched at 2022")
+                .price(70000)
+                .discountedPrice(65000)
+                .stock(true)
+                .live(true)
+                .productImageName("apple.png")
                 .build();
     }
 
+    private String convetObjectToJsonString(Object product) throws Exception {
+        try {
+            return new ObjectMapper().writeValueAsString(product);
 
+        } catch (Exception e) {
+            e.printStackTrace();
 
+            return null;
+        }
+    }
+//    create Product
+    @Test
+    public void createProduct_Test() throws Exception
+    {
+        ProductDto dto = this.mapper.map(product, ProductDto.class);
+
+        Mockito.when(this.productService.create(Mockito.any())).thenReturn(dto);
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(convetObjectToJsonString(product))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").exists());
+    }
 
 
 
