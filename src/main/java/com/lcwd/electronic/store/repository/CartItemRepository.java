66c5f67adaa3db41ead6ca2337f7c6e2319bfd44
pankajthakre1234 <pankajthakre786@gmail.com>
2023-07-12 20:19:21 +0530

    Successfully created cartItem Repository interface is done. code push by pankaj

---
+++ src/main/java/com/lcwd/electronic/store/repository/CartItemRepository.java
@@ -0,0 +1,9 @@
+package com.lcwd.electronic.store.repository;
+
+import com.lcwd.electronic.store.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem,Integer> {
+}
