37e086d7b248dab30a17ff07368f51bcff271416
pankajthakre1234 <pankajthakre786@gmail.com>
2023-07-13 14:22:04 +0530

    write a method in cart Service interface is done. code push by pankaj

---
+++ src/main/java/com/lcwd/electronic/store/helper/AddItemToCartRequest.java
@@ -0,0 +1,10 @@
+package com.lcwd.electronic.store.helper;
+
+
+import com.lcwd.electronic.store.dto.CartDto;
+
+public class AddItemToCartRequest {
+
+    private Integer productId;
+
+}
--- src/main/java/com/lcwd/electronic/store/service/CartService.java
+++ src/main/java/com/lcwd/electronic/store/service/CartService.java
@@ -1,5 +1,17 @@
 package com.lcwd.electronic.store.service;
 
 
+import com.lcwd.electronic.store.dto.CartDto;
+import com.lcwd.electronic.store.helper.AddItemToCartRequest;
+
 public interface CartService {
+
+//    added item to cart
+    CartDto addedItemToCart(Integer userId, AddItemToCartRequest request);
+
+//    remove item from cart
+    void removeItemFromCart(Integer userId, Integer cartItemId);
+
+//    clear cart
+    void clearCart(Integer userId);
 }
