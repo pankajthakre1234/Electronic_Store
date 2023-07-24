7ea9d49c6ba03fab644aaeb37512afc011468b9f
pankajthakre1234 <pankajthakre786@gmail.com>
2023-07-24 20:41:17 +0530

    Implementing The Loggers in All Method of Cart Controller class Successfully is done. code push by pankaj

--- src/main/java/com/lcwd/electronic/store/controller/CartController.java
+++ src/main/java/com/lcwd/electronic/store/controller/CartController.java
@@ -1,11 +1,12 @@
 package com.lcwd.electronic.store.controller;
 
 import com.lcwd.electronic.store.dto.CartDto;
-import com.lcwd.electronic.store.dto.UserDto;
 import com.lcwd.electronic.store.helper.AddItemToCartRequest;
 import com.lcwd.electronic.store.helper.ApiResponse;
 import com.lcwd.electronic.store.helper.AppConstant;
 import com.lcwd.electronic.store.service.CartService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +19,19 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
+    Logger logger = LoggerFactory.getLogger(CartController.class);
+
+
 //    create
 
     @PostMapping("/{userId}")
     public ResponseEntity<CartDto> addedItemToCart(@PathVariable Integer userId, @RequestBody AddItemToCartRequest request)
     {
+        logger.info("Initiate the request for Save the Item Details to cart with id :{}",userId);
+
         CartDto cartDto = this.cartService.addedItemToCart(userId, request);
 
+        logger.info("Completed the request for Save the Item Details to cart with id :{}",userId);
         return new ResponseEntity<>(cartDto, HttpStatus.CREATED);
     }
 
@@ -35,7 +42,9 @@ public class CartController {
             @PathVariable Integer cartItemId
     )
     {
+        logger.info("Initiate the request for Remove Item Details from Cart with user id :{} and cartItem id :{}",userId,cartItemId);
         this.cartService.removeItemFromCart(userId,cartItemId);
+        logger.info("Completed the request for Remove Item Details from Cart with user id :{} and cartItem id :{}",userId,cartItemId);
         return new ResponseEntity<>(new ApiResponse(AppConstant.CART_DELETED,true),HttpStatus.OK);
     }
 
@@ -43,7 +52,11 @@ public class CartController {
     @DeleteMapping("/{userId}")
     public ResponseEntity<ApiResponse> clearCart(@PathVariable Integer userId)
     {
+        logger.info("Initiate the request for clear cart Details with user id :{}",userId);
+
         this.cartService.clearCart(userId);
+        logger.info("Completed the request for clear cart Details with user id :{}",userId);
+
         return new ResponseEntity<>(new ApiResponse(AppConstant.CART_DELETED,true),HttpStatus.OK);
     }
 
@@ -51,8 +64,11 @@ public class CartController {
     @GetMapping("/user/{userId}")
     public ResponseEntity<CartDto> getCartByUser(@PathVariable Integer userId)
     {
+        logger.info("Initiate the request for get cart Details with user id :{}",userId);
+
         CartDto cartByUser = this.cartService.getCartByUser(userId);
 
+        logger.info("Completed the request for get cart Details with user id :{}",userId);
         return new ResponseEntity<>(cartByUser,HttpStatus.OK);
     }
 
