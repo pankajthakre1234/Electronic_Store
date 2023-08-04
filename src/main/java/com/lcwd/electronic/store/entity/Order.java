6676c74ef27d1089d08f3f11115ea25e1a27062b
pankajthakre1234 <pankajthakre786@gmail.com>
2023-08-04 19:38:33 +0530

    Create order and  Order Item  class is Successfully done. code push by pankaj

--- src/main/java/com/lcwd/electronic/store/entity/Order.java
+++ src/main/java/com/lcwd/electronic/store/entity/Order.java
@@ -1,4 +1,43 @@
 package com.lcwd.electronic.store.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer orderId;
+
+    private String orderStatus;
+
+    private String paymentStatus;
+
+    private int orderAmount;
+
+    private String billingAddress;
+
+    private String billingPhone;
+
+    private String billingName;
+
+    private Date orderDate;
+
+    private Date deliveredDate;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User user;
+
 }
