b738f0159f1b5c6bbb2f81d84504326209d442cd
pankajthakre1234 <pankajthakre786@gmail.com>
2023-08-08 20:11:58 +0530

    Create order Dto and OrderItemDto class is Successfully done. code push by pankaj

--- src/main/java/com/lcwd/electronic/store/dto/OrderDto.java
+++ src/main/java/com/lcwd/electronic/store/dto/OrderDto.java
@@ -1,4 +1,44 @@
 package com.lcwd.electronic.store.dto;
 
+import com.lcwd.electronic.store.entity.OrderItem;
+import com.lcwd.electronic.store.entity.User;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class OrderDto {
+
+    private Integer orderId;
+
+    private String orderStatus;
+
+    private String paymentStatus;
+
+    private int orderAmount;
+
+    @NotEmpty
+    @Column(length = 10000000)
+    private String billingAddress;
+
+    private String billingPhone;
+
+    private String billingName;
+
+    private Date orderDate;
+
+    private Date deliveredDate;
+
+    private UserDto user;
+
+    private List<OrderItem> orderItems= new ArrayList<>();
 }
--- src/main/java/com/lcwd/electronic/store/dto/OrderItemDto.java
+++ src/main/java/com/lcwd/electronic/store/dto/OrderItemDto.java
@@ -1,4 +1,28 @@
 package com.lcwd.electronic.store.dto;
 
+import com.lcwd.electronic.store.entity.Order;
+import com.lcwd.electronic.store.entity.Product;
+import lombok.*;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
 public class OrderItemDto {
+
+    private int OrderItemId;
+
+    private int quantity;
+
+    private int totalPrice;
+
+    private Product product;
+
+    private Order order;
 }
