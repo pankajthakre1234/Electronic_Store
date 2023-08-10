37cb066406074d2f4d7d6f18ba54e66b3b62b220
pankajthakre1234 <pankajthakre786@gmail.com>
2023-08-10 16:00:44 +0530

    In CreateOrderRequest class Are validation is Add Successfully is done. code push by pankaj

--- src/main/java/com/lcwd/electronic/store/helper/CreateOrderRequest.java
+++ src/main/java/com/lcwd/electronic/store/helper/CreateOrderRequest.java
@@ -5,6 +5,7 @@ import com.lcwd.electronic.store.entity.OrderItem;
 import lombok.*;
 
 import javax.persistence.Column;
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.Date;
@@ -17,14 +18,24 @@ import java.util.List;
 @ToString
 public class CreateOrderRequest {
 
+    @NotEmpty(message = "UserId Must Be required And  Not be empty")
     private Integer userId;
+
+    @NotEmpty(message = "cartId Must Be required And  Not be empty")
     private Integer cartId;
+
     private Integer orderId;
     private String orderStatus = "PENDING";
     private String paymentStatus= "NOT-PAID";
     private int orderAmount;
+
+    @NotEmpty(message = "Billing Address Must be Required and Proper Address")
     private String billingAddress;
+
+    @NotEmpty(message = "Billing Phone Number Must Be Required and number is On")
     private String billingPhone;
+
+    @NotEmpty(message = "Billing Name Must Be required And  Not be empty")
     private String billingName;
 
 }
